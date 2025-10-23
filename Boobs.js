ea0d36d2c5a46ef0bb49b9a4df91d335c076ee27
Lussyfer69 <nanasottu@gmail.com>
2025-10-23 10:11:55 +0530

    Boobs

---
+++ Boobs.js
@@ -0,0 +1,207 @@
+const files = [
+  "Boobs/-307367439.jpg",
+  "Boobs/-697679310 - Copy.jpg",
+  "Boobs/-697679310.jpg",
+  "Boobs/-1902287854.jpg",
+  "Boobs/-1960966662.jpg",
+  "Boobs/0a5e6af14521aa3309828b508be01e3d.jpg",
+  "Boobs/2d613cac7d94f615e6c0887c2ec25001.jpg",
+  "Boobs/2ec3aced94fd548a009dec37647e4e34.jpg",
+  "Boobs/3c0210fb308f44bf1ec831f1ac76b951.jpg",
+  "Boobs/3df53cd38d4721830c89f3efc1bf84e4.jpg",
+  "Boobs/4.png",
+  "Boobs/5c70c7f0b262dc4d3927f37067bc8f46.jpg",
+  "Boobs/6ea2dcd87cfd2eaf5bf9f793f19f221c.jpg",
+  "Boobs/9b59dbda86ee7c03a261e9482b1d8910.jpg",
+  "Boobs/9db0a049e0ff9f032368ac873e8bd5e3.jpg",
+  "Boobs/9fba28d1a31081dae9213ccd3a248f6a.jpg",
+  "Boobs/12fa6f62ea23dd1ebb5b05aa35133f51.jpg",
+  "Boobs/14dae156068838cc3344ee2e77abb3d4.jpg",
+  "Boobs/20dc0b33ffd9bab6535ec99543d8f784.jpg",
+  "Boobs/66be0577649a167ab414df707d351e77.jpg",
+  "Boobs/68e597c45a98983047675cd387f676a1.jpg",
+  "Boobs/84c854299f0fc1087d77108d8fe49302.jpg",
+  "Boobs/95f8f131bb9af289abbc09f7e1fc0b09.jpg",
+  "Boobs/98bed11cf4aca7ad8ae5da170dad43f8.jpg",
+  "Boobs/745d64261a22a248347568991298bfd5.jpg",
+  "Boobs/1002.jpg",
+  "Boobs/07025bad39f6ca2f723a94f7bd64baaa.jpg",
+  "Boobs/8093a279a0bdc4905fa2dd08510c5948.jpg",
+  "Boobs/9893aabd468f0e2674dce8e25f478a28.jpg",
+  "Boobs/10003.jpg",
+  "Boobs/10007.jpg",
+  "Boobs/10010.jpg",
+  "Boobs/10012.jpg",
+  "Boobs/74960a30693ee756fdf1031f28b6ffb8 - Copy.jpg",
+  "Boobs/557247cde879fb7f2d553e79cc8622e9.jpg",
+  "Boobs/2328065dd236e106395b67c90ace0c95.jpg",
+  "Boobs/93408809.jpg",
+  "Boobs/94301195.mp4",
+  "Boobs/376189012_980800669812144_7930568445297885633_n_17994639869501723.heic",
+  "Boobs/377337103_1000498781157666_889770743862758175_n_17999095631302371.heic",
+  "Boobs/681948834.jpg",
+  "Boobs/1538109344.jpg",
+  "Boobs/1598288056.jpg",
+  "Boobs/1998753067.jpg",
+  "Boobs/a782383694fd502ec74444d6d6a442f2.jpg",
+  "Boobs/a199142916579169723f1eb71de3b7f6.jpg",
+  "Boobs/ac2526228a734904ab71fa22a63d54ca.jpg",
+  "Boobs/aedd59efe3e81958e52e37da44ff0a15.jpg",
+  "Boobs/AfA7ScLrFkFzLE3D3CjNp2s-ve9TreLpgBxoA436s2N014lYHJL3w29jnKJRdDY.jpg",
+  "Boobs/AfAjgQshG6yDwfkVtm4i4_CToyScWLuABaDEChunWvbJcxykBYW8r8Up68EWNtQ.jpg",
+  "Boobs/AfD7lnk6GjCIor12fwiIDgu9LT21GEjC3havRu2imPeBZHCrZ_jmg1MFdwsW7w4.jpg",
+  "Boobs/AfDhbFTOD26LvdqEusblH6kAhodrAAbsJ1gmxvUl7JfdPgdgLHCbIrVRn8FGZTw.jpg",
+  "Boobs/b5f5c02d14a86ec5431595d31c194f74.jpg",
+  "Boobs/c1d962c77a2fe2beac7728176bc42ade.jpg",
+  "Boobs/c5374928f68e3b68a1bb6ab5ec589544.jpg",
+  "Boobs/ce748d7882278a3f70f6a35f0d29bd1c.jpg",
+  "Boobs/daa3db5ab8bebf9cbd7e684af3e94656.jpg",
+  "Boobs/ded0f54b98b87d83bdf6ec46047bd61d.jpg",
+  "Boobs/df450fcef04bde65fb9d0c45cf42e700.jpg",
+  "Boobs/e9ff8ce68f21034591f23fd7e335e661.jpg",
+  "Boobs/e76c6c7840e03fc32e3514ea7710699a.jpg",
+  "Boobs/Emrata_colors.jpg",
+  "Boobs/fbdf5d4d10e09cd4142b43b4446edf0f.jpg",
+  "Boobs/fe6ba1d9f12d695733cb3952d48843be.jpg",
+  "Boobs/fef8fd7cd9024c089681d5437aa60791.jpg",
+  "Boobs/g4ebyw057qvb1.jpeg",
+  "Boobs/image.jpg",
+  "Boobs/IMG_5986.png",
+  "Boobs/IMG_5988.png",
+  "Boobs/IMG_6942.png",
+  "Boobs/IMG_8215.jpg",
+  "Boobs/IMG_20240815_004611_164.jpg",
+  "Boobs/Instagram_a6ee38474e644b318c6f4846cc7f6b.jpg",
+  "Boobs/kate-upton-getting-outta-in-topless-optimized.jpg",
+  "Boobs/kate-upton-leaked-boobs-optimized.jpg",
+  "Boobs/Kate-Upton-nude-hot-sexy-topless-bikini-feet-porn-ass-boob-pussy-ScandalPlanet-.jpg",
+  "Boobs/Kate-Upton-SI-2017-6-300x300.jpg",
+  "Boobs/Kate-Upton-SI-2017-11-300x300.jpg",
+  "Boobs/millie_bobby_brown_see_thru_nips.jpg",
+  "Boobs/Millie-Bobby-Brown-nude-topless-porn-sexy-tits-pussy-ass-feet-boyfriend-bikini-no-makeup-ScandalPlan.jpg",
+  "Boobs/MTV-1886206561854374046-01.jpg",
+  "Boobs/olivia-munn_2149.jpg",
+  "Boobs/Olivia-Munn-Esquire-6.jpg",
+  "Boobs/Olivia-Munn-white-top-cleavage.jpg",
+  "Boobs/olivia.png",
+  "Boobs/olivia2.png",
+  "Boobs/oliviarodrigo_0a47fd7fd29846a690078e365616c64d.jpg",
+  "Boobs/RDT_20240229_124236346870069499758478.jpg",
+  "Boobs/RDT_20240229_1240536121312650135743220.jpg",
+  "Boobs/RDT_20240229_1243435179743758865611592.jpg",
+  "Boobs/RDT_20241015_1502493404754948013655087.jpg",
+  "Boobs/RDT_20250713_1042494420374249825926209.jpg",
+  "Boobs/Screenshot_20240114_122338_Instagram.jpg",
+  "Boobs/snapins-ai_3691774908280984786.jpg",
+  "Boobs/SnapInsta.to_499934871_18520097551013912_710621476274765555_n.jpg",
+  "Boobs/sydney_sweeney_nude_the_voyeurs.jpg",
+  "Boobs/sydney_sweeney4.jpg",
+  "Boobs/the-sexiest-bikini-there-ever-will-be-unbeatable-period-v0-fhc49ydy3cgf1.jpg",
+  "Boobs/x161448_tk1_02526-rawweb1920jpg.jpg",
+  "Boobs/x161448_tk1_02532-rawweb1920jpg.jpg",
+  "Boobs/x161448_tk1_02604-rawweb1920jpg.jpg",
+];
+
+const gallery = document.getElementById("gallery");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const modalVideo = document.getElementById("modal-video");
+const closeBtn = document.getElementsByClassName("close")[0];
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const prevDirBtn = document.getElementById("prev-dir");
+const nextDirBtn = document.getElementById("next-dir");
+let currentIndex = 0;
+
+const directories = [
+  "Alia",
+  "Ass",
+  "Boobs",
+  "Brook",
+  "Disha",
+  "Kiara",
+  "Madelyn",
+  "Margot",
+  "Millie",
+  "Munn",
+  "Olivia",
+  "Swimsuit Illustrated",
+  "Syd",
+  "kate",
+  "Celebsex",
+];
+const currentDir = "Boobs";
+const currentDirIndex = directories.indexOf(currentDir);
+
+files.forEach((file, index) => {
+  const item = document.createElement("div");
+  item.className = "gallery-item";
+  const isVideo = file.endsWith(".mp4");
+  if (isVideo) {
+    const video = document.createElement("video");
+    video.src = file;
+    video.muted = true; // Mute for autoplay
+    video.loop = true;
+    video.playsInline = true;
+    item.appendChild(video);
+  } else {
+    const img = document.createElement("img");
+    img.src = file;
+    img.alt = file;
+    item.appendChild(img);
+  }
+  item.addEventListener("click", () => {
+    currentIndex = index;
+    showModal(currentIndex);
+  });
+  gallery.appendChild(item);
+});
+
+function showModal(index) {
+  const file = files[index];
+  const isVideo = file.endsWith(".mp4");
+  modal.style.display = "block";
+  if (isVideo) {
+    modalImg.style.display = "none";
+    modalVideo.style.display = "block";
+    modalVideo.src = file;
+  } else {
+    modalVideo.style.display = "none";
+    modalImg.style.display = "block";
+    modalImg.src = file;
+  }
+}
+
+prevBtn.onclick = function () {
+  currentIndex = currentIndex > 0 ? currentIndex - 1 : files.length - 1;
+  showModal(currentIndex);
+};
+
+nextBtn.onclick = function () {
+  currentIndex = currentIndex < files.length - 1 ? currentIndex + 1 : 0;
+  showModal(currentIndex);
+};
+
+prevDirBtn.onclick = function () {
+  const prevIndex =
+    currentDirIndex > 0 ? currentDirIndex - 1 : directories.length - 1;
+  window.location.href = directories[prevIndex] + ".html";
+};
+
+nextDirBtn.onclick = function () {
+  const nextIndex =
+    currentDirIndex < directories.length - 1 ? currentDirIndex + 1 : 0;
+  window.location.href = directories[nextIndex] + ".html";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+  modalVideo.pause();
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modalVideo.pause();
+  }
+};
